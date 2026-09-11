c40ba8ca705624cad7fced4a2b468c033ca20c7b
inventor01 <austinrock2000@gmail.com>
2026-09-11 12:35:22 -0400

    Use one primary dashboard and remove duplicate page stacks

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { requireChatGPTUser } from './chatgpt-auth';
 import BrowserIntelligence from './browser-intelligence';
-import Desk from './desk';
 import DiscoveryDashboard from './discovery-dashboard';
 import FrontLiveTools from './front-live-tools';
-import NarrativeCreationWatcher from './narrative-creation-watcher';
 import RadarManager from './radar-manager';
 
 export default async function Home() {
   await requireChatGPTUser('/');
-  return <><DiscoveryDashboard/><Desk/><BrowserIntelligence/><NarrativeCreationWatcher/><FrontLiveTools/><RadarManager/></>;
+  return <><DiscoveryDashboard/><BrowserIntelligence/><FrontLiveTools/><RadarManager/></>;
 }
