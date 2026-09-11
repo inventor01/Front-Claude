4ff9609759d15ae481a855fb97458de8cf752b23
inventor01 <austinrock2000@gmail.com>
2026-09-11 02:53:05 -0400

    Enable automatic narrative launch matching

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import BrowserDashboardSync from './browser-dashboard-sync';
 import BrowserIntelligence from './browser-intelligence';
 import Desk from './desk';
 import FrontLiveTools from './front-live-tools';
+import NarrativeCreationWatcher from './narrative-creation-watcher';
 
 export default async function Home() {
   await requireChatGPTUser('/');
-  return <><Desk/><BrowserIntelligence/><BrowserDashboardSync/><FrontLiveTools/></>;
+  return <><Desk/><BrowserIntelligence/><BrowserDashboardSync/><NarrativeCreationWatcher/><FrontLiveTools/></>;
 }
