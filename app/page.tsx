7fc7566d4c2bfa1331a5dad2bf7336b11dbd6b05
inventor01 <austinrock2000@gmail.com>
2026-09-11 04:21:51 -0400

    Mount first-class Public Signals

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ import BrowserIntelligence from './browser-intelligence';
 import Desk from './desk';
 import FrontLiveTools from './front-live-tools';
 import NarrativeCreationWatcher from './narrative-creation-watcher';
+import PublicSignalsPanel from './public-signals-panel';
 import RadarManager from './radar-manager';
 
 export default async function Home() {
   await requireChatGPTUser('/');
-  return <><Desk/><BrowserIntelligence/><BrowserDashboardSync/><NarrativeCreationWatcher/><FrontLiveTools/><RadarManager/></>;
+  return <><Desk/><PublicSignalsPanel/><BrowserIntelligence/><BrowserDashboardSync/><NarrativeCreationWatcher/><FrontLiveTools/><RadarManager/></>;
 }
