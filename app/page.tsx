436135d7feb6aa342c3d7970cff273990198b4e9
inventor01 <austinrock2000@gmail.com>
2026-09-11 14:03:50 -0400

    Remove duplicate Radar Manager from the primary workspace

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import { requireChatGPTUser } from './chatgpt-auth';
 import BrowserIntelligence from './browser-intelligence';
 import DiscoveryDashboard from './discovery-dashboard';
 import FrontLiveTools from './front-live-tools';
-import RadarManager from './radar-manager';
 
 export default async function Home() {
   await requireChatGPTUser('/');
-  return <><DiscoveryDashboard/><BrowserIntelligence/><FrontLiveTools/><RadarManager/></>;
+  return <><DiscoveryDashboard/><BrowserIntelligence/><FrontLiveTools/></>;
 }
